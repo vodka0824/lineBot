5259bb8ddbf96c081700f37ac0fb7121e3829ea2
vodka0824” <vodka0825@gmail.com>
2026-01-01 02:11:14 +0800

    fix(line): fix syntax error - add missing closing brace

--- utils/line.js
+++ utils/line.js
@@ -60,18 +60,20 @@ async function pushMessage(to, messages) {
         console.error('推播訊息失敗:', error.response?.data || error.message);
         throw error; // 讓呼叫者知道失敗
     }
-    /**
-     * 主動推播 Flex 訊息
-     */
-    async function pushFlex(to, alt, flex) {
-        await pushMessage(to, [{ type: 'flex', altText: alt, contents: flex }]);
-    }
+}
 
-    module.exports = {
-        replyToLine,
-        replyText,
-        replyFlex,
-        getGroupMemberName,
-        pushMessage,
-        pushFlex
-    };
+/**
+ * 主動推播 Flex 訊息
+ */
+async function pushFlex(to, alt, flex) {
+    await pushMessage(to, [{ type: 'flex', altText: alt, contents: flex }]);
+}
+
+module.exports = {
+    replyToLine,
+    replyText,
+    replyFlex,
+    getGroupMemberName,
+    pushMessage,
+    pushFlex
+};
