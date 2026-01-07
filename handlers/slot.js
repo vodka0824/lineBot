732b79adf34b49e36df4103ec5dabee3009b7012
vodka0824” <vodka0825@gmail.com>
2026-01-07 17:57:34 +0800

    feat: update slot images to private GCS bucket

--- handlers/slot.js
+++ handlers/slot.js
@@ -8,8 +8,8 @@ const { replyFlex } = require('../utils/line');
 // 符號清單 (對應圖片目錄中的符號名稱)
 const SYMBOLS = ['7', 'apple', 'bar', 'bell', 'cherry', 'diamond', 'grape', 'lemon', 'orange', 'plum', 'watermelon'];
 
-// 圖片資源 Base URL (暫時借用對方的 GitHub Pages 或 Raw 連結)
-const IMG_BASE = 'https://raw.githubusercontent.com/mant0u0/LineBot-Mant0u/main/static/images/randomSlot';
+// 圖片資源 Base URL (已掛載至您的 Google Cloud Storage)
+const IMG_BASE = 'https://storage.googleapis.com/my-linebot-assets/slot';
 
 // 中獎線路定義 (8條線的索引)
 // 0 1 2
